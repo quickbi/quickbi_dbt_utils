ed9cd098e014121299c96d1b48e7d800df7d768d
Eero Vehmanen <eero.vehmanen@gmail.com>
2020-10-31 20:59:40 +0200

    Add comment

--- macros/get_audit_schema.sql
+++ macros/get_audit_schema.sql
@@ -1,3 +1,11 @@
+-- Set schema name for package "fishtown-analytics/logging"
+-- (https://github.com/fishtown-analytics/dbt-event-logging).
+--
+-- Note that in the main dbt package there needs to be a macro like this:
+--
+-- {% macro get_audit_schema() %}
+--     {{ return(quickbi_dbt_utils.get_audit_schema()) }}
+-- {% endmacro %}
 {% macro get_audit_schema() %}
     {{ return(target.schema ~ '_warehouse_meta') }}
 {% endmacro %}
