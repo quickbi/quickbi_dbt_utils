5aa6a4e72a7a0db0fc52427f054c66e1510f194d
Eero Vehmanen <eero.vehmanen@gmail.com>
2020-11-23 10:23:00 +0200

    Fix error caused by comment
    Running dbt raised the following error even though the other "macro" was inside a comment.
    
    'dbt found two macros named "get_audit_schema" in the project "quickbi_dbt_utils".'

--- macros/get_audit_schema.sql
+++ macros/get_audit_schema.sql
@@ -1,11 +1,9 @@
 -- Set schema name for package "fishtown-analytics/logging"
 -- (https://github.com/fishtown-analytics/dbt-event-logging).
 --
--- Note that in the main dbt package there needs to be a macro like this:
---
--- {% macro get_audit_schema() %}
---     {{ return(quickbi_dbt_utils.get_audit_schema()) }}
--- {% endmacro %}
+-- Note that in the main dbt package there needs to be a macro with the same
+-- name as this calling this macro like this:
+--   {{ return(quickbi_dbt_utils.get_audit_schema()) }}
 {% macro get_audit_schema() %}
     {{ return(target.schema ~ '_warehouse_meta') }}
 {% endmacro %}
